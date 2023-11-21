8ac64b2819462e5516afee04fb449bc3b6cfcda4
MiracleChibuike <miraclechibuike27@gmail.com>
2023-11-21 20:44:53 +0100

    name the app

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// Defining our Settings variable
+// This variable will hold the settings container
+let allFeatures = document.querySelector(".AllContactsContainer");
+let AllNames = document.querySelectorAll(".contactName");
+// let headre = document.querySelector(".contactName")
+let manner = document.querySelector(".ale");
+let phoneOthers = document.querySelector(".phoneMobile")
+let AllContent = document.body;
+var appBody = document.querySelector(".mainContainer");
+const PhoneSettings = document.getElementById("case");
+// PhoneSettings.addEventListener("click", function () {
+ 
+
+//     if (AllContent.style.background = "#010101") {
+//         AllContent.style.background = "#f1f1f1"
+//         manner.style.color = "#010101"
+//         phoneOthers.style.color = "#010101";
+        
+//     }
+
+// });
